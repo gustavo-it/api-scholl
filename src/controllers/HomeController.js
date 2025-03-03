51f578ad7929d60583ab8c05d44cb93332c8c42e
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-03-03 20:06:36 -0300

    CRUD of users

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -2,15 +2,12 @@ import Student from "../models/Student";
 
 class HomeController {
   async index(request, response) {
-    const newStudent = await Student.create({
-      name: "Maria",
-      lastName: "Barbosa",
-      email: "maria@gmail.com",
-      age: 24,
-      weight: 60,
-      height: 1.70,
-    });
-    response.json(newStudent);
+    try {
+      const students = await Student.findAll();
+      return response.json(students);
+    } catch (error) {
+      return response.json(null);
+    }
   }
 }
 
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,13 +4,69 @@ class UserController {
   async store(request, response) {
     try {
       const newUser = await User.create(request.body);
-      response.json(newUser);
+      return response.json(newUser);
     } catch (error) {
-      response.status(400).json({
+      return response.status(400).json({
         errors: error.errors ? error.errors.map(error => error.message) : ["Unknow Error"],
       });
     }
   }
+
+  async index(request, response) {
+    try {
+      const users = await User.findAll();
+      return response.json(users);
+    } catch (error) {
+      return response.json(null);
+    }
+  };
+
+  async show(request, response) {
+    try {
+      const { id } = request.params;
+      const user = await User.findByPk(id);
+      return response.json(user);
+    } catch (error) {
+      return response.json(null);
+    }
+  };
+
+  async update(request, response) {
+    try {
+      const { id } = request.params;
+      if (!id) return response.status(400).json({ errors: ["ID not sent."] });
+
+      const user = await User.findByPk(id);
+
+      if (!user) return response.status(400).json({ errors: ["User not found."] });
+
+      user.update(request.body);
+      return response.json(user);
+
+    } catch (error) {
+      return response.json(null);
+    }
+  };
+  
+  async delete(request, response) {
+    try {
+      const { id } = request.params;
+
+      if(!id) return response.status(400).json({ errors: ["ID not sent."] });
+
+      const user = await User.findByPk(id);
+
+      if(!user) return response.status(400).json({ errors: ["User not found."] });
+
+      user.destroy();
+
+      return response.json({
+        information: `User: ${user.name}, e-mail: ${user.email} was deleted`
+      });
+    } catch(error) {
+      return response.json(null);
+    }
+  };
 }
 
 export default new UserController();
